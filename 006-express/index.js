fdbbb49ccbee40812b4d76bdd7cf96c5374ed03e
Solo-Cat <sir.noen@yandex.ru>
2023-08-28 13:36:06 +0300

    testing..

--- 006-express/index.js
+++ 006-express/index.js
@@ -8,7 +8,7 @@ class Book {
     title = "Над пропастью во Ржи",
     description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique diam vel vestibulum consequat. Proin cursus nunc a erat egestas, vel aliquet tortor semper. Nullam magna mi, condimentum non ex ac, maximus pellentesque nulla. Nam sed euismod magna, blandit imperdiet erat. Nunc tincidunt feugiat odio viverra tempor. Etiam a pretium magna. Nulla quis nunc eu mauris tincidunt pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut vitae orci eu tortor tincidunt fermentum quis eu mauris. Praesent congue ipsum quis bibendum laoreet. Sed vel egestas velit. Etiam pellentesque risus ultrices, bibendum mi sit amet, ultrices nisl.",
     authors = "Джером Дэвид Сэлинджер",
-    favorite = "Yes",
+    favorite = true,
     fileCover = "",
     fileName = "Selindger_book.pdf"
   ) {
